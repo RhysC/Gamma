0a629c37a3b7fac433329ae45cf8f7a86300e545
Rhys Campbell <rhysevancampbell@gmail.com>
2012-04-19 07:14:06 +0100

    initial commit pending some JS work to dynamically work the cubes

---
+++ WebClient/assets/js/cube.js
@@ -0,0 +1,38 @@
+function create_cube()
+{
+	//create a cube dom element
+	/*
+	<div class="cube" style="float: left;">
+		<div class="product">
+			EUR/USD
+		</div>
+		<div class="bid">
+			<div class="title">
+				Bid
+			</div>
+			<div class="amount">
+				1.31208
+			</div>
+		</div>
+		<div class="ask">
+			<div class="title">
+				Ask
+			</div>
+			<div class="amount">
+				1.31228
+			</div>
+		</div>
+	</div>
+	*/
+	
+	//get cube continer div
+	$("#cubes")....
+	//insert cube into DOM
+	
+	//QUnit tests?
+}
+
+function subscribe_to_prices(cube)
+{
+	
+}
